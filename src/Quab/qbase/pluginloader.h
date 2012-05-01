d56f8ba4c10dccf84eb0fe69a2adf7291070a794
drzunny <drzunny@hotmail.com>
2012-05-02 00:14:32 +0800

    remove old file in qbase, the lastest qbase is written by C

--- src/Quab/qbase/pluginloader.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLATFORM_H
-#define PLATFORM_H
-
-void qbase_plugin_init(wchar_t* path);
-void* qbase_plugin_load(char* pluginName, char* module);
-void  qbase_plugin_unload();
-
-#endif // PLATFORM.h
