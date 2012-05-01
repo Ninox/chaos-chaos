a3784ea505abfb75e301e70d1bb1fb4434b31f27
drzunny <drzunny@hotmail.com>
2012-05-02 00:16:23 +0800

    finished the basic feature of basescript(except to qbase_lua_call, qbase_lua_reg)

--- src/Quab/qbase/basescript.h
+++ src/Quab/qbase/basescript.h
@@ -1,22 +1,49 @@
-#ifndef BASESCRIPT_H
-#define BASESCRIPT_H
+#ifndef QBASE_LUA_H
+#define QBASE_LUA_H
 
-typedef void* (*qbase_function)();
-typedef char ret_status;
-union qbase_return  {
-    int iVal;
-    double dVal;
-    char* strVal;
-    bool bVal;
+#include <stddef.h>
+#include <lua.h>
+
+enum qbase_ret_type	{
+		NIL	= 0,
+		NUMBER = 1,
+		BOOLEAN=  2,
+		STRING = 3,
+		TABLE = 4,
+		UNKNOW = 5
 };
 
-ret_status qbase_script_init();
-ret_status qbase_script_execute(char* src, int retcount, void** ret);
-ret_status qbase_script_load(char* path);
-void qbase_script_reg(char* name, qbase_function f);
-qbase_return qbase_script_get(char* valname);
-qbase_return qbase_script_getfield(char* table, char* field);
-void qbase_script_close();
+typedef struct qbase_str	{
+	const char* str;
+	size_t len;
+} qbase_str;
 
+typedef struct qbase_tbl	{
+	const char** data;
+	size_t size;
+} qbase_tbl;
 
-#endif  //basescript.h
+typedef struct qbase_ret	{
+	union	{
+		double number_val;
+		int bool_val;
+		qbase_str str_val;
+		qbase_tbl* tbl_val;
+	} val;
+	int ret_type;
+} qbase_ret;
+
+typedef void* (*qbase_regfunc)(lua_State* L, void* params, size_t pcnt);
+
+typedef struct qbase_sta qbase_sta;
+
+int qbase_lua_init(qbase_sta** sta);
+void qbase_lua_close(struct qbase_sta* sta);
+int qbase_lua_exec(char* text, const char* chunk_name, qbase_sta* sta);
+int qbase_lua_load(char* file, const char* chunk_name, qbase_sta* sta);
+void qbase_lua_reg(qbase_regfunc f, qbase_sta* sta);
+qbase_ret* qbase_lua_call(const char* func_name, const qbase_ret* params, size_t paramcnt, size_t retcnt, qbase_sta* sta);
+qbase_ret qbase_lua_get(const char* name, qbase_sta* sta);
+qbase_ret qbase_lua_getfield(const char* tbname, const char* fieldname, qbase_sta* sta);
+
+#endif
