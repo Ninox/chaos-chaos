aa7e6514fef5733c20a5840d7197d6a8e44bb5fe
drzunny <drzunny@hotmail.com>
2012-06-02 16:14:51 +0800

    ADD: basepacker test

---
+++ test/basepacker/test.c
@@ -0,0 +1,28 @@
+#include "basepacker.h"
+#include <assert.h>
+
+int test_compress()	{
+	return 1;
+}
+
+int test_uncompress()	{
+	return 1;
+}
+
+int test_setpwd()	{
+	return 1;
+}
+
+int test_update()	{
+	return 1;
+}
+
+int test_remove(){
+	return 1;
+}
+
+int main()
+{
+
+	return 0;
+}
