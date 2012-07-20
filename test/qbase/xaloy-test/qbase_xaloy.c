fbf489418d6e42015526c7b4155fdf728695083e
drzunny <drzunny@hotmail.com>
2012-07-20 16:06:02 +0800

    update the qbase_xaloy.c file

--- test/qbase/xaloy-test/qbase_xaloy.c
+++ test/qbase/xaloy-test/qbase_xaloy.c
@@ -17,23 +17,38 @@
 
 /*   helper for this binding to get table   */
 static void
-get_vector(lua_State *L, float *x, float *y)    {
-    lua_pushstring(L, "x");
-    lua_gettable(L, -2);
-    *x = (float)lua_tonumber(L,-1);
+set_vector(lua_State *L, qbase_vector *v)	{
+	lua_pushnumber(L, v->posX);
+	lua_setfield(L, -2, "x");
+	lua_pushnumber(L, v->posY);
+	lua_setfield(L, -2, "y");
+}
+
+static void
+get_vector(lua_State *L, qbase_vector *v)    {
+    lua_getfield(L, -1, "x");
+    v->posX = (float)lua_tonumber(L,-1);
     lua_pop(L, 1);
-    lua_pushstring(L, "y");
-    lua_gettable(L, -2)
-    *y = (float)lua_tonumber(L,-1);
+    lua_getfield(L, -1, "y");
+    v->posY = (float)lua_tonumber(L,-1);
     lua_pop(L, 1);
 }
 
 static void
-get_matrix(lua_State *L, float *m)    {
+set_matrix(lua_State *L, qbase_matrix2 *mtx)	{
+	int i;
+	for(i = 0; i < 4; i++)	{
+		lua_pushnumber(L, mtx->m[i/2][i%2]);
+		lua_rawseti(L, -2, i+1);
+	}
+}
+
+static void
+get_matrix(lua_State *L, qbase_matrix2 *mtx)    {
 	int i;
 	for(i = 0; i < 4; i++)	{
 		lua_rawgeti(L, -1, i+1);
-		m[i] = (float)lua_tonumber(L, -1);
+		mtx->m[i/2][i%2] = (float)lua_tonumber(L, -1);
 		lua_pop(L, 1);
 	}
 }
@@ -55,15 +70,15 @@ qmath_equal(lua_State *L)    {
 }
 
 static int
-qvector_plus(lua_State *L)    {    
+qvector_plus(lua_State *L)    {
     float v1x, v1y, v2x, v2y;
-    
+
     get_vector(L, &v2x, &v2y);
     lua_pushvalue(L, -2);
-    
+
     get_vector(L, &v1x, &v1y);
     lua_pop(L, 1);
-    
+
     /*    create a new table    */
     lua_newtable(L);
     lua_pushstring(L, "x");
@@ -78,14 +93,14 @@ static int
 qvector_dot(lua_State *L)    {
     float v1x, v1y, v2x, v2y;
     qbase_vector v1, v2;
-    
+
     get_vector(L, &v2x, &v2y);
     lua_pushvalue(L, -2);
-    
+
     get_vector(L, &v1x, &v1y);
     lua_pop(L, 1);
-    
-    /*    calculate the dot operation    */    
+
+    /*    calculate the dot operation    */
     v1.posX = v1x; v1.posY = v1y;
     v2.posX = v2x; v2.posY = v2y;
     lua_pushnumber(L, qbase_vector_dot(v1, v2))
@@ -96,14 +111,14 @@ static int
 qvector_cos(lua_State *L)    {
     float v1x, v1y, v2x, v2y;
     qbase_vector v1, v2;
-    
+
     get_vector(L, &v2x, &v2y);
     lua_pushvalue(L, -2);
-    
+
     get_vector(L, &v1x, &v1y);
     lua_pop(L, 1);
-    
-    /*    calculate the dot operation    */    
+
+    /*    calculate the dot operation    */
     v1.posX = v1x; v1.posY = v1y;
     v2.posX = v2x; v2.posY = v2y;
     lua_pushnumber(L, qbase_vector_cos(v1, v2))
@@ -114,14 +129,14 @@ static int
 qvector_sin(lua_State *L)    {
     float v1x, v1y, v2x, v2y;
     qbase_vector v1, v2;
-    
+
     get_vector(L, &v2x, &v2y);
     lua_pushvalue(L, -2);
-    
+
     get_vector(L, &v1x, &v1y);
     lua_pop(L, 1);
-    
-    /*    calculate the dot operation    */    
+
+    /*    calculate the dot operation    */
     v1.posX = v1x; v1.posY = v1y;
     v2.posX = v2x; v2.posY = v2y;
     lua_pushnumber(L, qbase_vector_sin(v1, v2))
@@ -132,14 +147,14 @@ static int
 qvector_isparallel(lua_State *L)    {
     float v1x, v1y, v2x, v2y;
     qbase_vector v1, v2;
-    
+
     get_vector(L, &v2x, &v2y);
     lua_pushvalue(L, -2);
-    
+
     get_vector(L, &v1x, &v1y);
     lua_pop(L, 1);
-    
-    /*    calculate the dot operation    */    
+
+    /*    calculate the dot operation    */
     v1.posX = v1x; v1.posY = v1y;
     v2.posX = v2x; v2.posY = v2y;
     lua_pushinteger(L, qbase_vector_isparallel(v1, v2))
@@ -150,14 +165,14 @@ static int
 qvector_isvertical(lua_State *L)    {
     float v1x, v1y, v2x, v2y;
     qbase_vector v1, v2;
-    
+
     get_vector(L, &v2x, &v2y);
     lua_pushvalue(L, -2);
-    
+
     get_vector(L, &v1x, &v1y);
     lua_pop(L, 1);
-    
-    /*    calculate the dot operation    */    
+
+    /*    calculate the dot operation    */
     v1.posX = v1x; v1.posY = v1y;
     v2.posX = v2x; v2.posY = v2y;
     lua_pushinteger(L, qbase_vector_isvertical(v1, v2))
@@ -168,9 +183,9 @@ static int
 qvector_length(lua_State *L)    {
     float v1x, v1y;
     qbase_vector v1;
-    
-    /*    calculate the dot operation    */        
-    get_vector(L, &v1x, &v1y);    
+
+    /*    calculate the dot operation    */
+    get_vector(L, &v1x, &v1y);
     v1.posX = v1x; v1.posY = v1y;
     lua_pushinteger(L, qbase_vector_length(v1))
     return 1;
@@ -182,13 +197,13 @@ qvector_mul(lua_State *L)    {
     qbase_vector v;
     /*    get the k value    */
     k = (float)lua_checknumber(L, -1);
-    
+
     /*    get vector    */
     lua_pushvalue(L, -2);
     get_vector(L, &vx, &vy);
     lua_pop(L, 1);
     v.posX = vx; v.posY = vy;
-    
+
     /*    get the qbase' API returns */
     ret = qbase_vector_mul(&v, k);
     lua_newtable(L);
@@ -198,72 +213,163 @@ qvector_mul(lua_State *L)    {
     lua_pushnumber(L, v.posY);
     lua_settable(L, -5);
     lua_pushinteger(L, ret);
-    
-    return 2;    
+
+    return 2;
 }
 
 static int
 qmatrix_init(lua_State *L)    {
+	qbase_matrix2 mtx, ret;
+	get_matrix(L, &mtx);
+	qbase_matrix_init(&ret, 0, mtx.m[0][0],mtx.m[0][1],mtx.m[1][0],mtx.m[1][1]);
+
+	lua_newtable(L);
+	set_matrix(L, &ret);
+	return 1;
 }
 
 static int
 qmatrix_zero(lua_State *L)    {
+	qbase_matrix2 mtx;
+	int result = -1;
+	qbase_matrix_zero(&mtx, ROW_BASE);
+
+	if(mtx.m[0][0] == 0 && mtx.m[0][1] == 0 && mtx.m[1][0] == 0 && mtx.m[1][1] == 0)	{
+		result = 1;
+	}
+	else result = 0;
+	lua_pushboolean(L, result);
+
+	return 1;
 }
 
 static int
 qmatrix_plus(lua_State *L)    {
 	int i;
-	float m1[4], m2[4];
 	qbase_matrix2 mtx1, mtx2, ret;
 	/*	get matrix data	*/
-	get_matrix(L, m2);
+	get_matrix(L, mtx2);
 	lua_pushvalue(L, -2);
-	get_matrix(L, m1);
+	get_matrix(L, mtx1);
 	lua_pop(L, 1);
-	
-	/*	create matrix	*/	
-	mtx1.m[0][0] = m1[0];mtx1.m[0][1] = m1[1];
-	mtx1.m[1][0] = m1[2];mtx1.m[1][1] = m1[3];
-	mtx2.m[0][0] = m2[0];mtx2.m[0][1] = m2[1];
-	mtx2.m[1][0] = m2[2];mtx2.m[1][1] = m2[3];
-	
+
 	/*  calculate the result and return it  */
 	ret = qbase_matrix_plus(&mtx1, &mtx2);
 	lua_newtable(L);
-	for(i = 0; i < 4; i++)	{
-		lua_pushnumber(L, ret.m[i/2][i%2]);
-		lua_rawseti(L, -2, i+1);
-	}
-	
+	set_matrix(L, &ret);
+
 	return 1;
 }
 
 static int
 qmatrix_trans(lua_State *L)    {
+	qbase_matrix2 mtx;
+	/*  get matrix and call trans function  */
+	get_matrix(L, &mtx);
+	qbase_matrix_trans(&mtx);
+
+	/*  return matrix  */
+	lua_newtable(L);
+	set_matrix(L, &mtx);
+
+	return 1;
 }
 
 static int
 qmatrix_adjoint(lua_State *L)    {
+	qbase_matrix2 mtx, ret;
+	/*  get matrix and call trans function  */
+	get_matrix(L, &mtx);
+	ret = qbase_matrix_adjoint(&mtx);
+
+	/*  return matrix  */
+	lua_newtable(L);
+	set_matrix(L, &ret);
+
+	return 1;
 }
 
 static int
 qmatrix_inverse(lua_State *L)    {
+	qbase_matrix2 mtx;
+	/*  get matrix and call trans function  */
+	get_matrix(L, &mtx);
+	qbase_matrix_inverse(&mtx);
+
+	/*  return matrix  */
+	lua_newtable(L);
+	set_matrix(L, &mtx);
+
+	return 1;
 }
 
 static int
 qmatrix_det(lua_State *L)    {
+	qbase_matrix2 mtx;
+	float det;
+
+	/*  get matrix data and calculate the det  */
+	get_matrix(L, &mtx);
+	det = qbase_matrix_det(&mtx);
+
+	/*  return the det value  */
+	lua_pushnumber(L, det);
+	return 1;
 }
 
 static int
 qmatrix_extend(lua_State *L)    {
+	qbase_matrix2 mtx;
+	float k;
+	/*  the lua table is the first parameters of this function  */
+	lua_pushvalue(L, -2);
+	get_matrix(L, &mtx);
+	lua_pop(L, 1);
+	k = lua_tonumber(L, -1);
+	qbase_matrix_extend(&mtx, k);
+
+	/*  return matrix data  */
+	lua_newtable(L);
+	set_matrix(L, &mtx);
+
+	return 1;
 }
 
 static int
 qmatrix_mul(lua_State *L)    {
+	qbase_matrix2 mtx1, mtx2, ret;
+
+	/*  get matrix data  */
+	get_matrix(L, &mtx2);
+	lua_pushvalue(L, -2);
+	get_matrix(L, &mtx1);
+	lua_pop(L, 1);
+
+	/*  return the mul operation's return  */
+	ret = qbase_matrix_mul(&mtx1, &mtx2);
+	lua_newtable(L);
+	set_matrix(L, &ret);
+
+	return 1;
 }
 
 static int
 qmatrix_vmul(lua_State *L)    {
+	qbase_vector v, ret;
+	qbase_matrix mtx;
+
+	/*  get arguments data  */
+	get_matrix(L, &mtx);
+	lua_pushvalue(L, -2);
+	get_vector(L, &v);
+	lua_pop(L, 1);
+	ret = qbase_matrix_vmul(&v, &mtx);
+
+	/*  return the vmul result  */
+	lua_newtable(L);
+	set_vector(L, &ret);
+
+	return 1;
 }
 
 /*---------- baseloader API binding ----------*/
@@ -398,26 +504,26 @@ qpacker_rename(lua_State *L)	{
 /*----------------------  register to Lua ----------------------*/
 static const
 struct luaL_Reg qbase_apis[] = {
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
+    {"qmath_todegree",qmath_todegree},
+    {"qmath_equal",qmath_equal},
+    {"qvector_plus",qvector_plus},
+    {"qvector_dot", qvector_dot},
+    {"qvector_cos", qvector_cos},
+    {"qvector_sin",qvector_sin},
+    {"qvector_isparallel", qvector_isparallel},
+    {"qvector_isvertical", qvector_isvertical},
+    {"qvector_length", qvector_length},
+    {"qvector_mul", qvector_mul},
+    {"qmatrix_init", qmatrix_init},
+    {"qmatrix_zero", qmatrix_zero},
+    {"qmatrix_plus", qmatrix_plus},
+    {"qmatrix_trans", qmatrix_trans},
+    {"qmatrix_adjoint", qmatrix_adjoint},
+    {"qmatrix_inverse", qmatrix_inverse},
+    {"qmatrix_det", qmatrix_det},
+    {"qmatrix_extend", qmatrix_extend},
+    {"qmatrix_mul",qmatrix_mul},
+    {"qmatrix_vmul",qmatrix_vmul},
     {},
     {},
     {},
