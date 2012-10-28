27fe41f5520ce96ecd70da5113deb6a560afb6e8
drzunny <drzunny@hotmail.com>
2012-10-28 23:55:19 +0800

    update the get & set method in QuabLua

--- src/Quab/Lua/QuabLua.h
+++ src/Quab/Lua/QuabLua.h
@@ -1,30 +1,57 @@
 #ifndef QUAB_LUA_H
 #define QUAB_LUA_H
 
+#include <string>
 #include <vector>
-#include "QuabLuaValue.h"
+#include <map>
 
 struct lua_State;
 
 namespace Quab
 {
 	class QuabStream;
-      typedef int (*QuabLuaCallback)(lua_State *L);
+    class QuabLuaTable;
+    typedef int (*QuabLuaCallback)(lua_State *L);
+    typedef const char* QuabLuaKeyString;
 	
-	class QuabLuaTable
+    struct QuabLuaObject 
+    {
+        union   {
+            double number;
+            bool boolean;
+            std::string *str;
+            QuabLuaTable *table;
+        } _value;
+
+        int type;
+    };
+
+	class QUAB_API QuabLuaTable
 	{
 	private:
+        std::vector<QuabLuaObject> _luaArray;        
+        std::map<const char*, QuabLuaObject> _luamap;
+        QuabLuaTable(const QuabLuaTable &table){}
+
 	public:
+        QuabLuaTable();
+        ~QuabLuaTable();
+        QuabLuaObject& operator[](unsigned idx);
+        QuabLuaObject& operator[](const char *name);
+        unsigned getLength();
+        QuabLuaKeyString* getKeys(unsigned *cnt);
 	};	
 	
-	class QuabLua
+	class QUAB_API QuabLua
 	{
 	private:
 		lua_State *L;
-            QuabLua(const QuabLua &ql){}
+        QuabLua(const QuabLua &lua){}
+        QuabLua(){}
 	public:
-            QuabLua();
-            ~QuabLua();
+        static QuabLua* create(const char *vmName);
+        static void release(const char *vmName);
+        ~QuabLua();
 
 		void set(const char *name, int value);
 		void set(const char *name, double value);
@@ -34,14 +61,17 @@ namespace Quab
 		
 		bool exists(const char *name, int *type);
 		
-		QuabLuaValue get(const char *name);	
+        bool get(const char *name, double *value);
+        bool get(const char *name, std::string *str);
+        bool get(const char *name, bool *value);
+        bool get(const char *name, QuabLuaTable *table);
 	
 		bool exec(const QuabStream *buffer, bool isString = true);
 		bool exec(const char *file);
 		
 		bool registerTo(QuabLuaCallback f, const char *name);
 		
-		void call(const char *fname, const std::vector<QuabLuaValue> &param);
+		void call(const char *fname, const QuabLuaTable &params);
 				
 	};
 }
--- src/Quab/QuabLua.cc
+++ src/Quab/QuabLua.cc
@@ -1,19 +1,92 @@
 #include "resource/QuabResource.h"
 #include "Lua/QuabLua.h"
+#include <stdlib.h>
 #include <lua.hpp>
 
 using namespace Quab;
 
-QuabLua::QuabLua()      {
-      this->L = luaL_newstate();
-      luaL_openlibs(this->L);
+/*    helper methods    */
+static bool
+luahelper_checkexist(lua_State *L, const char *name, int *type)    {
+      lua_getglobal(L, name);
+      bool flag = false;
+      if(type == NULL)  {
+          flag = lua_type(L, -1) != LUA_TNIL;
+      } else    {
+          flag = lua_type(L, -1) != *type;
+      }
+      lua_pop(L, 1);
+      return flag;
 }
 
+inline static bool
+luahelper_checkgetter(lua_State *L, const char *name, int type) {
+    lua_getglobal(L, name);
+    return type == lua_type(L, -1);
+}
+
+/*    the impletement of the QuabLuaTable    */
+QuabLuaTable::QuabLuaTable()    {
+    this->_luaArray.reserve(10);
+}
+QuabLuaTable::~QuabLuaTable()   {
+    this->_luamap.clear();
+    this->_luaArray.clear();
+    this->_luaArray.reserve(1);
+}
+
+QuabLuaObject& QuabLuaTable::operator[](unsigned idx)   {
+    if(idx >= this->_luaArray.size())
+        throw "over the max index of Lua array";
+    else 
+        return this->_luaArray[idx];
+}
+
+QuabLuaObject& QuabLuaTable::operator[](const char *name)   {
+    return this->_luamap[name];
+}
+
+unsigned QuabLuaTable::getLength()  {
+    return this->_luaArray.size();
+}
+
+QuabLuaKeyString* QuabLuaTable::getKeys(unsigned *cnt)   {
+    unsigned counter = 0;
+    std::map<const char*, QuabLuaObject>::const_iterator iter;
+    QuabLuaKeyString *keyArray = (QuabLuaKeyString*)malloc(sizeof(QuabLuaKeyString) * this->_luamap.size());
+    for(iter = this->_luamap.begin(); iter != this->_luamap.end(); iter++)  {
+        keyArray[counter] = (*iter).first;
+        counter++;
+    }
+    *cnt = counter;
+    return keyArray;
+}
+
+/*    the impletement of QuabLua    */
+static std::map<const char*, QuabLua*> __luaPool;
+
 QuabLua::~QuabLua()     {
-      if(this->L != NULL)     {
-            lua_close(this->L);
-            this->L = NULL;
-      }
+    this->L = NULL;
+}
+
+QuabLua* QuabLua::create(const char *vmName)  {    
+    // search the vm in map
+    if(__luaPool.count(vmName) > 0)    {
+        return __luaPool[vmName];
+    } else  {
+        lua_State *L = luaL_newstate();
+        luaL_openlibs(L);
+        QuabLua *lua = new QuabLua();
+        lua->L = L;
+        __luaPool[vmName] = lua;
+        return lua;
+    }
+}
+
+void QuabLua::release(const char *vmName) {
+    if(__luaPool.count(vmName) > 0) {
+        __luaPool.erase(vmName);
+    }
 }
 
 void QuabLua::set(const char *name, int value)  {
@@ -41,43 +114,56 @@ void QuabLua::set(const char *name, const QuabLuaTable &value)  {
 }
 
 bool QuabLua::exists(const char *name, int *type)      {
-      lua_getglobal(this->L, name);
-      bool flag = false;
-      if(lua_type(this->L, -1) != LUA_TNIL)     {
-            flag = true;
-      }
-      lua_pop(this->L, 1);
-      return flag;
+     return luahelper_checkexist(this->L, name, type);
 }
 
-QuabLuaValue QuabLua::get(const char *name)     {
-      lua_getglobal(this->L, name);
-      QuabLuaValue lv;
-      lv.LuaValueType = Quab::VALUE_UNKNOW;
-      switch(lua_type(this->L, -1)) {
-            case LUA_TNIL:
-                  lv.LuaValueType = Quab::VALUE_NIL;
-                  lua_pop(this->L, 1);
-                  break;
-            case LUA_TNUMBER:
-                  lv = QuabLuaValue(lua_tonumber(this->L, -1));
-                  lua_pop(this->L, 1);
-                  return lv;
-            case LUA_TSTRING:
-                  lv = QuabLuaValue(lua_tostring(this->L, -1));
-                  lua_pop(this->L, 1);
-                  break;
-            case LUA_TBOOLEAN:
-                  lv = QuabLuaValue(lua_toboolean(this->L, -1) == 1);
-                  lua_pop(this->L, 1);
-                  return lv;
-            case LUA_TTABLE:
-                  break;
-            default:
-                  break;
-      }
+bool QuabLua::get(const char *name, double *value)  {
+    bool flag;
+    if(!luahelper_checkgetter(this->L, name, LUA_TNUMBER))
+        flag = false;
+    else    {
+        *value = lua_tonumber(this->L, -1);
+        flag = true;
+    }
+    lua_pop(this->L, 1);
+    return flag;
+}
 
-      return lv;
+bool QuabLua::get(const char *name, std::string *str)    {
+    bool flag;
+    if(!luahelper_checkgetter(this->L, name, LUA_TSTRING))
+        flag = false;
+    else    {
+        const char *_str = lua_tostring(this->L, -1);
+        *str = _str;
+        flag = true;
+    }
+    lua_pop(this->L, 1);
+    return flag;
+}
+
+bool QuabLua::get(const char *name, bool *value)    {
+    bool flag;
+    if(!luahelper_checkgetter(this->L, name, LUA_TBOOLEAN))
+        flag = false;
+    else    {
+        *value = lua_toboolean(this->L, -1) == 1;
+        flag = true;
+    }
+    lua_pop(this->L, 1);
+    return flag;
+}
+
+bool QuabLua::get(const char *name, QuabLuaTable *table)    {
+    bool flag;
+    if(!luahelper_checkgetter(this->L, name, LUA_TTABLE))
+        flag = false;
+    else    {
+        //*table = lua_tonumber(this->L, -1);
+        flag = true;
+    }
+    lua_pop(this->L, 1);
+    return flag;
 }
 
 bool QuabLua::exec(const QuabStream *buffer, bool isString)    {
@@ -108,6 +194,7 @@ bool QuabLua::registerTo(QuabLuaCallback f, const char *name)    {
       lua_register(this->L, name, f);
 }
 
-std::vector<QuabLuaValue> QuabLua::call(const char *fname, const std::vector<QuabLuaValue> &param)    {
+void QuabLua::call(const char *fname, const QuabLuaTable &params)
+{
       
 }
